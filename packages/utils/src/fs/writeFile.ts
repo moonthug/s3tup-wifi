b1866dc6581d882d3e0bfa81bc4a10908f43a401
Alex Coulcher <alex@polyglot.rodeo>
2021-02-14 13:07:51 +0000

    updates

--- packages/utils/src/fs/writeFile.ts
+++ packages/utils/src/fs/writeFile.ts
@@ -1,7 +1,11 @@
 import { promises as fs } from 'fs';
+import path from 'path';
+import mkdirp from 'mkdirp';
 
 export async function writeFile(filepath: string, data: unknown): Promise<void> {
   let fileData = data.toString();
+
+  await mkdirp(path.dirname(filepath));
   
   if (typeof data === 'object') {
     fileData = JSON.stringify(data);
